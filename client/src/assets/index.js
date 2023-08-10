e7992d0e89b6cb8630d1ff8193a5d7b7c18c8fa6
Santiago Pulido Diaz <santiagopulido12@gmail.com>
2023-08-10 19:51:14 +0200

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,15 @@
+import download from './download.png';
+import logo from './logo.svg';
+import logo2 from './logo2.svg';
+import svg from './svg.svg';
+import preview from './preview.png';
+
+
+
+export {
+  download,
+  svg,
+  logo,
+  logo2,
+  preview,
+};
