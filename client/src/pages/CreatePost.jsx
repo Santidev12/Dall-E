58cae61419e12d6cc449fc0a0fc5300914313f4f
Santiago Pulido Diaz <santiagopulido12@gmail.com>
2023-08-10 20:41:58 +0200

    server deployed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
     if(form.prompt){
       try {
         setgeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://dall-e-7i0o.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
