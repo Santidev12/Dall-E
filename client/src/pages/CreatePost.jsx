26062f3b3a5f769358d154867851b92d1f142f89
Santiago Pulido Diaz <santiagopulido12@gmail.com>
2023-08-10 21:29:25 +0200

    fetch fixed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -46,7 +46,7 @@ const CreatePost = () => {
     if(form.prompt && form.photo){
       setloading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://dall-e-7i0o.onrender.com/api/v1/post", {
           method:"POST",
           headers:{
             'Content-Type': 'application/json',
